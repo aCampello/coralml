60f3bbb1c3acdd2b122e388203ae20dc3e96e584
Johannes Blaschke <jpblaschke@lbl.gov>
2020-12-01 10:30:00 -0800

    typo

--- docker/tests/test_nvtorch.py
+++ docker/tests/test_nvtorch.py
@@ -11,4 +11,4 @@ print(f" * Number of GPUS: {n_device}")
 print(" * Available devices:")
 for i in range(n_device):
     d_name = torch.cuda.get_device_name(i)
-    print("     {i}: {d_name}")
+    print(f"     {i}: {d_name}")
